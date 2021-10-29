3795829ffc4d39e481e0dea88ac9b1e7b0be8187
Reky Bongso <rekybongso@gmail.com>
2021-10-29 20:05:50 +0800

    start building profile feature

--- app/src/main/java/com/rekyb/jyro/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/rekyb/jyro/ui/profile/ProfileFragment.kt
@@ -1,3 +1,17 @@
 package com.rekyb.jyro.ui.profile
 
-class ProfileFragment
+import android.os.Bundle
+import android.view.View
+import com.rekyb.jyro.R
+import com.rekyb.jyro.databinding.FragmentProfileBinding
+import com.rekyb.jyro.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.tvPlaceholder?.text = getString(R.string.label_profile)
+    }
+}
