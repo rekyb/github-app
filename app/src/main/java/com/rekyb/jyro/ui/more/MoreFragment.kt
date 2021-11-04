71f6eadfb4987107cd8af4ce14550c7334c2666d
Reky Bongso <rekybongso@gmail.com>
2021-11-04 18:02:22 +0800

    start developing settings features

--- app/src/main/java/com/rekyb/jyro/ui/more/MoreFragment.kt
+++ app/src/main/java/com/rekyb/jyro/ui/more/MoreFragment.kt
@@ -1,18 +1,14 @@
 package com.rekyb.jyro.ui.more
 
 import android.os.Bundle
-import android.view.View
+
+import androidx.preference.PreferenceFragmentCompat
 import com.rekyb.jyro.R
-import com.rekyb.jyro.databinding.FragmentMoreBinding
-import com.rekyb.jyro.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MoreFragment : BaseFragment<FragmentMoreBinding>(R.layout.fragment_more) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding?.tvPlaceholder?.text = activity?.application?.getString(R.string.label_more)
+class MoreFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.pref_screen_main, rootKey)
     }
 }
